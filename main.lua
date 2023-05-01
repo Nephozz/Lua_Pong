6b3e647b20a25191f63e7981da3aa14870a6f6be
Thomas Bocandé <thomasbocande@gmail.com>
2023-05-01 20:33:55 +0200

    Implémentation du mouvement des raquettes Ajout de l'affichage du score

--- main.lua
+++ main.lua
@@ -1,18 +1,18 @@
 --[[
-    Test Pong version 2
+    Test Pong version 3
     23/01/2022
 
     "test du language lua et de LöVE2D"
 
     -- Main Program --
-    
+
     Auteur: Thomas Bocandé
-    
+
     #Changelog:
 
         --version 0 (23/01/2022):
             Affichage du texte 'Hello World!'
-        
+
         --version 1 (23/01/2022):
             Affichage basse résolution
             Ajout de la commande 'quitter l'application'
@@ -22,24 +22,35 @@
             Modification du texte 'Hello World!' en 'Hello Pong!' et déplacement en haut
             Ajout des deux raquettes et de la balle
 
+        --version 3 (23/01/2022):
+            Implémentation du mouvement des raquettes
+            Ajout de l'affichage du score
+
 ]]
 
 push = require 'push'                             -- importation de la librarie 'push'
 
+-- taille de la fenêtre
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+-- taille de la fenêtre virtuelle
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
+
+-- vitesse de la raquette
+PADDLE_SPEED = 200
 
 --[[ Initialisation du jeu ]]
 
 function love.load()
-
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     -- police type rétro
     smallFont = love.graphics.newFont('font.ttf', 8)
+
+    -- police pour le score
+    scoreFont = love.graphics.newFont('font.ttf', 32)
 
     love.graphics.setFont(smallFont)
 
@@ -49,9 +60,35 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+    --scores des joueurs
+    player1score = 0
+    player2score = 0
+
+    -- positions initiales des raquettes
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
 end
 
---[[ Commandes ]]
+--[[ Update du jeu ]]
+
+function love.update(dt)
+    -- mouvement du joueur 1
+    if love.keyboard.isDown('z') then
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+    
+    --mouvement du joueur 2
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+end
+
+--[[ Commandes supplémentaires ]]
 
 function love.keypressed(key)
 
@@ -67,18 +104,24 @@ function love.draw()
 
     -- début du rendu à basse résolution
     push:apply('start')
-    
+
     -- nettoye l'écran avec une couleur proche de celle de l'arrière plan du Pong
     love.graphics.clear(40, 45, 52, 0.2)
 
     -- affiche 'Hello Pong!' en haut de l'écran
+    love.graphics.setFont(smallFont)
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
+    --affiche les scores
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
     -- dessine la raquette de gauche
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
     -- dessine la raquette de droite
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
     -- dessine la balle
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
